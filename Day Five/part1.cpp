0e9857a0d627d97ecdc7569e25bc32a56ffeab94
Thomas Braccia <bracciata@gmail.com>
2020-12-06 15:43:23 -0600

    Finished P1 Day Five

--- Day Five/part1.cpp
+++ Day Five/part1.cpp
@@ -2,17 +2,36 @@
 #include <fstream>
 #include <string>
 #include <array>
+#include <cmath>
 
 using namespace std;
 
-int getSeatID(string binarySpace, int currentMinRow,int currentMaxRow,int currentMinColumn,int currentMaxColumn){
-    if(binarySpace[0]=='F'){
-        return getSeatID(binarySpace,currentMinRow,floor(currentMaxRow/2)+currentMinRow,currentMinColumn,currentMaxColumn);    
+int getSeatID(string binarySpace, int currentMinRow, int currentMaxRow, int currentMinColumn, int currentMaxColumn)
+{
+    if (binarySpace[0] == 'F')
+    {
+        return getSeatID(binarySpace, currentMinRow, floor(currentMaxRow / 2) + currentMinRow, currentMinColumn, currentMaxColumn);
     }
-    if(binarySpace[0]=='F'){
-        return getSeatID(binarySpace,currentMinRow,floor(currentMaxRow/2),currentMinColumn,currentMaxColumn);    
+    if (binarySpace[0] == 'F')
+    {
+        return getSeatID(binarySpace, currentMinRow, floor(currentMaxRow / 2), currentMinColumn, currentMaxColumn);
     }
 }
+
+//https://www.programiz.com/cpp-programming/examples/binary-decimal-convert
+int convertBinaryToDecimal(long n)
+{
+    int decimalNumber = 0, i = 0, remainder;
+    while (n != 0)
+    {
+        remainder = n % 10;
+        n /= 10;
+        decimalNumber += remainder * pow(2, i);
+        ++i;
+    }
+    return decimalNumber;
+}
+
 // main() is where program execution begins.
 int main()
 {
@@ -20,15 +39,51 @@ int main()
     fstream inputFile;
     // Reading a file from https://www.tutorialspoint.com/read-data-from-a-text-file-using-cplusplus#:~:text=Read%20Data%20from%20a%20Text%20File%20using%20C%2B%2B,operation%20using%20object%20newfile.%204%20Example%205%20Output
     inputFile.open("input.txt", ios::in); //open a file to perform read operation using file object
-    int maxID=0;
+    int maxID = 0;
     if (inputFile.is_open())
     { //checking whether the file is open
-        string tp;
+        std::string tp;
         while (getline(inputFile, tp))
         {
             //read data from file object and put it into string.
             cout << tp << "\n";
-            int seatID = getSeatID(tp,0,127);
+            size_t pos = 0;
+            while ((pos = tp.find('F', pos)) != std::string::npos)
+            {
+                tp.replace(pos, 1, "0");
+                pos += 1;
+            }
+            pos = 0;
+            while ((pos = tp.find('B', pos)) != std::string::npos)
+            {
+                tp.replace(pos, 1, "1");
+                pos += 1;
+            }
+            cout << tp << "\n";
+            std::string rowBinary = tp.substr(0, 7);
+            cout << rowBinary << '\n';
+            int row = convertBinaryToDecimal(std::stol(rowBinary));
+            std::string colStr = tp.substr(7, 3);
+            cout << colStr << '\n';
+            pos = 0;
+            while ((pos = colStr.find('L', pos)) != std::string::npos)
+            {
+                colStr.replace(pos, 1, "0");
+                pos += 1;
+            }
+            pos = 0;
+            while ((pos = colStr.find('R', pos)) != std::string::npos)
+            {
+                colStr.replace(pos, 1, "1");
+                pos += 1;
+            }
+            int col = convertBinaryToDecimal(std::stol(colStr));
+            if ((row * 8) + col > maxID)
+            {
+                maxID = (row * 8) + col;
+            }
+        }
+        cout << "Max ID: " << maxID << '\n';
+        return 0;
     }
-    return 0;
 }

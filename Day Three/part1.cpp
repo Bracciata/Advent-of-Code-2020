5508c90202d83e9406e4ec8215eaa124ddaec7ee
Thomas Braccia <bracciata@gmail.com>
2020-12-06 15:20:57 -0600

    Updates?

--- Day Three/part1.cpp
+++ Day Three/part1.cpp
@@ -3,32 +3,33 @@
 #include <string>
 using namespace std;
 
-// main() is where program execution begins.
 int main()
 {
-    int valid = 0;
     fstream inputFile;
-    // Reading a file from https://www.tutorialspoint.com/read-data-from-a-text-file-using-cplusplus#:~:text=Read%20Data%20from%20a%20Text%20File%20using%20C%2B%2B,operation%20using%20object%20newfile.%204%20Example%205%20Output
-    inputFile.open("input.txt", ios::in); //open a file to perform read operation using file object
+    inputFile.open("input.txt", ios::in);
 
     if (inputFile.is_open())
-    { //checking whether the file is open
+    {
         string tp;
         int lineNumber = 0;
-        int trees = 1;
+        int trees = 0;
         while (getline(inputFile, tp))
         {
-            //read data from file object and put it into string.
-            if(lineNumber>0){
-                int spotToCheck = (3 * lineNumber) % (tp.length()-1);
+            if (lineNumber > 0)
+            {
+                int spotToCheck = (3 * lineNumber) % (tp.length() - 1);
 
-                if(tp[spotToCheck]=='#'){
+                if (tp[spotToCheck] == '#')
+                {
                     trees++;
+                    cout << lineNumber << endl;
+                    
                 }
+
             }
             lineNumber++;
         }
-        inputFile.close(); //close the file object.
+        inputFile.close();
         cout << "Total Trees: " << trees << "\n";
     }
     return 0;
--- Day Three/part2.cpp
+++ Day Three/part2.cpp
@@ -6,16 +6,15 @@ using namespace std;
 int getTrees(int x, int y)
 {
     fstream inputFile;
-    // Reading a file from https://www.tutorialspoint.com/read-data-from-a-text-file-using-cplusplus#:~:text=Read%20Data%20from%20a%20Text%20File%20using%20C%2B%2B,operation%20using%20object%20newfile.%204%20Example%205%20Output
-    inputFile.open("input.txt", ios::in); //open a file to perform read operation using file object
+    inputFile.open("input.txt", ios::in); 
     int trees = 0;
     if (inputFile.is_open())
-    { //checking whether the file is open
+    { 
         string tp;
         int lineNumber = 0;
         while (getline(inputFile, tp))
         {
-            //read data from file object and put it into string.
+         
             if (lineNumber > 0 && lineNumber % y == 0)
             {
                 int spotToCheck = (x * lineNumber) % (tp.length() - 1);
@@ -27,13 +26,12 @@ int getTrees(int x, int y)
             }
             lineNumber++;
         }
-        inputFile.close(); //close the file object.
+        inputFile.close(); 
         cout << "Total Trees: " << trees << "\n";
     }
     return trees;
 }
 
-// main() is where program execution begins.
 int main()
 {
 
